4ceb71962e47e0769342344035cb5eb134bc3ddd
allisoncwalker <82103388+allisoncwalker@users.noreply.github.com>
2022-10-11 10:18:53 -0500

    In class edits

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -3,7 +3,36 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        LinkedList<string> linkedList = new LinkedList<string>();
+
+        linkedList.AddLast("xbx");
+        linkedList.AddLast("pka");
+        linkedList.AddLast("pka");
+        linkedList.AddLast("xbx");
+
+        var reverse = linkedList.Reverse();
+        var currentNode = linkedList.First;
+        var currentNodeReverse = reverse.First();
+
+        //while (currentNode != null)
+        //{
+
+        //    currentNode = currentNode.Next;
+        //}
+
+        foreach(var node in linkedList)
+        {
+            Console.WriteLine(node);
+        }
+        Console.WriteLine( reverse.Equals( linkedList ));
     }
+}
+
+public static bool IsPalindrome<T>(LinkedList<T> linkedList)
+{
+    // Are the first and last items the same?
+
+    // if so, move towards the middle.
+    return true;
 }
 
